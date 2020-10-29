69f8d7f559d97c202b33317d72644fe63784b52e
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-29 11:57:12 +0500

    Updat App.css and App.js
    Updated code to use a little of css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 // step 03
 function App() {
   const [isLit, setLit] = useState(true);
+  const brightness = isLit ? "lit" : "dark";
 
   return (
-    <div>
+    <div className={`room ${brightness}`}>
       the room is {isLit ? 'lit' : 'dark'}
       <br></br>
-      {/* by using arrow function wigthin the onClick call we can delay the flip functionalty with our will */}
       <button onClick={()=>setLit(!isLit)}>
         Flip
       </button>
@@ -17,28 +17,44 @@ function App() {
   );
 }
 
-// // step 02
+// // step 03
 // function App() {
 //   const [isLit, setLit] = useState(true);
 
 //   return (
 //     <div>
 //       the room is {isLit ? 'lit' : 'dark'}
+//       <br></br>
+//       {/* by using arrow function wigthin the onClick call we can delay the flip functionalty with our will */}
+//       <button onClick={()=>setLit(!isLit)}>
+//         Flip
+//       </button>
 //     </div>
 //   );
 // }
 
-// // // step 01
+// // // step 02
 // // function App() {
 // //   const [isLit, setLit] = useState(true);
-// //   // use state can be destructured as follow
-// //   // const state = React.useState(true);
-// //   // const isLit = state[0];
-// //   // const setLit = state[1];
 
-// // return (
-// //   <div className="room">the room is lit</div>
-// // );
+// //   return (
+// //     <div>
+// //       the room is {isLit ? 'lit' : 'dark'}
+// //     </div>
+// //   );
 // // }
+
+// // // // step 01
+// // // function App() {
+// // //   const [isLit, setLit] = useState(true);
+// // //   // use state can be destructured as follow
+// // //   // const state = React.useState(true);
+// // //   // const isLit = state[0];
+// // //   // const setLit = state[1];
+
+// // // return (
+// // //   <div className="room">the room is lit</div>
+// // // );
+// // // }
 
 export default App;
