36bcb0e3c47ff18df92ad2954d9c0f04a5443013
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-29 11:40:28 +0500

    Update App.js
    used use state with out updating value

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React , {useState} from 'react';
 import './App.css';
 
-// step 01
+// step 02
 function App() {
-    const [isLit, setLit] = useState(true);
-    // use state can be destructured as follow
-    // const state = React.useState(true);
-    // const isLit = state[0];
-    // const setLit = state[1];
+  const [isLit, setLit] = useState(true);
 
   return (
-    <div className="room">the room is lit</div>
+    <div>
+      the room is {isLit ? 'lit' : 'dark'}
+    </div>
   );
 }
+
+// // step 01
+// function App() {
+//   const [isLit, setLit] = useState(true);
+//   // use state can be destructured as follow
+//   // const state = React.useState(true);
+//   // const isLit = state[0];
+//   // const setLit = state[1];
+
+// return (
+//   <div className="room">the room is lit</div>
+// );
+// }
 
 export default App;
