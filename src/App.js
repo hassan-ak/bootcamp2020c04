79559e83c2dafffd016be62dfb7d2d18e3661e66
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-29 11:37:21 +0500

    Update App.js
    basic santyx of useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React , {useState} from 'react';
 import './App.css';
 
+// step 01
 function App() {
+    const [isLit, setLit] = useState(true);
+    // use state can be destructured as follow
+    // const state = React.useState(true);
+    // const isLit = state[0];
+    // const setLit = state[1];
+
   return (
-    <div>
-      Hello World
-    </div>
+    <div className="room">the room is lit</div>
   );
 }
 
