89d6b786424b8a50495fe937cd762041bd87d19c
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-29 11:58:46 +0500

    Update App.js
    used className by directly calling a comparison

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React , {useState} from 'react';
 import './App.css';
 
-// step 03
+// step 05
 function App() {
   const [isLit, setLit] = useState(true);
-  const brightness = isLit ? "lit" : "dark";
 
   return (
-    <div className={`room ${brightness}`}>
+    <div className={`room ${isLit ? "lit" : "dark"}`}>
       the room is {isLit ? 'lit' : 'dark'}
       <br></br>
       <button onClick={()=>setLit(!isLit)}>
@@ -17,15 +16,15 @@ function App() {
   );
 }
 
-// // step 03
+// // step 04
 // function App() {
 //   const [isLit, setLit] = useState(true);
+//   const brightness = isLit ? "lit" : "dark";
 
 //   return (
-//     <div>
+//     <div className={`room ${brightness}`}>
 //       the room is {isLit ? 'lit' : 'dark'}
 //       <br></br>
-//       {/* by using arrow function wigthin the onClick call we can delay the flip functionalty with our will */}
 //       <button onClick={()=>setLit(!isLit)}>
 //         Flip
 //       </button>
@@ -33,28 +32,44 @@ function App() {
 //   );
 // }
 
-// // // step 02
+// // // step 03
 // // function App() {
 // //   const [isLit, setLit] = useState(true);
 
 // //   return (
 // //     <div>
 // //       the room is {isLit ? 'lit' : 'dark'}
+// //       <br></br>
+// //       {/* by using arrow function wigthin the onClick call we can delay the flip functionalty with our will */}
+// //       <button onClick={()=>setLit(!isLit)}>
+// //         Flip
+// //       </button>
 // //     </div>
 // //   );
 // // }
 
-// // // // step 01
+// // // // step 02
 // // // function App() {
 // // //   const [isLit, setLit] = useState(true);
-// // //   // use state can be destructured as follow
-// // //   // const state = React.useState(true);
-// // //   // const isLit = state[0];
-// // //   // const setLit = state[1];
 
-// // // return (
-// // //   <div className="room">the room is lit</div>
-// // // );
+// // //   return (
+// // //     <div>
+// // //       the room is {isLit ? 'lit' : 'dark'}
+// // //     </div>
+// // //   );
 // // // }
+
+// // // // // step 01
+// // // // function App() {
+// // // //   const [isLit, setLit] = useState(true);
+// // // //   // use state can be destructured as follow
+// // // //   // const state = React.useState(true);
+// // // //   // const isLit = state[0];
+// // // //   // const setLit = state[1];
+
+// // // // return (
+// // // //   <div className="room">the room is lit</div>
+// // // // );
+// // // // }
 
 export default App;
