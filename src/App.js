fad24a66458273b7283c467ac7eccfab1fa16118
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-10-29 11:51:46 +0500

    Update App.js
    UseState completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React , {useState} from 'react';
 import './App.css';
 
-// step 02
+// step 03
 function App() {
   const [isLit, setLit] = useState(true);
 
   return (
     <div>
       the room is {isLit ? 'lit' : 'dark'}
+      <br></br>
+      {/* by using arrow function wigthin the onClick call we can delay the flip functionalty with our will */}
+      <button onClick={()=>setLit(!isLit)}>
+        Flip
+      </button>
     </div>
   );
 }
 
-// // step 01
+// // step 02
 // function App() {
 //   const [isLit, setLit] = useState(true);
-//   // use state can be destructured as follow
-//   // const state = React.useState(true);
-//   // const isLit = state[0];
-//   // const setLit = state[1];
 
-// return (
-//   <div className="room">the room is lit</div>
-// );
+//   return (
+//     <div>
+//       the room is {isLit ? 'lit' : 'dark'}
+//     </div>
+//   );
 // }
+
+// // // step 01
+// // function App() {
+// //   const [isLit, setLit] = useState(true);
+// //   // use state can be destructured as follow
+// //   // const state = React.useState(true);
+// //   // const isLit = state[0];
+// //   // const setLit = state[1];
+
+// // return (
+// //   <div className="room">the room is lit</div>
+// // );
+// // }
 
 export default App;
